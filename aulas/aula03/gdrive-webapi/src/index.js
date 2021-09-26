8a7fa38b323b14242ffb1b75cbc6d21c9905db43
luan.correia <luan.correia@intelie.com.br>
2021-09-26 20:50:53 -0300

    Prepare evironment for deploy heroku

--- aulas/aula03/gdrive-webapi/src/index.js
+++ aulas/aula03/gdrive-webapi/src/index.js
@@ -1,21 +1,27 @@
 import https from 'https'
+import http from 'http'
 import fs from 'fs'
 import {logger} from './logger.js'
 import {Server} from 'socket.io'
 import Routes from './routes.js'
 
 const PORT = process.env.PORT || 3000
+const  isProduction = process.env.NODE_ENV === 'production'
+process.env.USER = process.env.USER ?? 'system_user'
+
 
 const localhostSSL = {
     key: fs.readFileSync('./certificates/key.pem'),
     cert: fs.readFileSync('./certificates/cert.pem')
     
 }
+const protocol = isProduction ? http : https
+const sslConfig = isProduction ? {} : localhostSSL
 
 const routes = new Routes()
 
-const server = https.createServer(
-    localhostSSL,
+const server = protocol.createServer(
+    sslConfig,
     routes.handler.bind(routes)
     )
     
@@ -32,7 +38,8 @@ const server = https.createServer(
     
     const startServer = ()=>{
         const {address, port} = server.address()
-        logger.info(`app running at https://${address}:${port}`)
+        const protocol = isProduction ? 'http':'https'
+        logger.info(`app running at ${protocol}://${address}:${port}`)
     }
     
     server.listen(PORT, startServer)
